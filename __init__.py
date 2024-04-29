b6de87ee7c390dad8e8c09ec5a7b38c67857d9ea
sabrinezz <165166750+sabrinezz@users.noreply.github.com>
2024-04-29 16:33:46 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -50,7 +50,17 @@ def Readfiche(post_id):
     return render_template('read_data.html', data=data)
 
 
-@app.route('/fiche_nom/<nom>')
+@app.route('/fiche_nom/<nom>', methods=['GET', 'POST'])
+def authentification2():
+    if request.method == 'POST':
+        # Vérifier les identifiants
+        if request.form['username'] == 'user' and request.form['password'] == '12345': # password à cacher par la suite
+            session['authentifie'] = True
+            # Rediriger vers la route lecture après une authentification réussie
+            return redirect(url_for('read_data.html'))
+        else:
+            # Afficher un message d'erreur si les identifiants sont incorrects
+            return render_template('formulaire_authentification.html', error=True)
 def Readfichenom(nom):
     conn = sqlite3.connect('database.db')
     cursor = conn.cursor()
@@ -59,6 +69,12 @@ def Readfichenom(nom):
     conn.close()
     # Rendre le template HTML et transmettre les données
     return render_template('read_data.html', data=data)
+
+
+
+
+
+
 
 @app.route('/consultation/')
 def ReadBDD():
